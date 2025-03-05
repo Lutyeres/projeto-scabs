5f21f627b748ecc787c4c5b4729ecf7246731091
Savyo Lutyeres <lutyeres@gmail.com>
2025-03-05 19:36:11 -0300

    Classe de Domínio

---
+++ SCABS/src/main/java/com/lutysoft/scabs/modelo/dominio/faccao.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lutysoft.scabs.modelo.dominio;
+
+/**
+ *
+ * @author lutye
+ */
+public class faccao {
+    
+}
