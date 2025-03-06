173d12895b76ace789af31986b778ee986b2f561
Savyo Lutyeres <lutyeres@gmail.com>
2025-03-06 13:28:12 -0300

    Criação da classe DAO

---
+++ SCABS/src/main/java/com/lutysoft/scabs/modelo/dao/FaccaoDao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lutysoft.scabs.modelo.dao;
+
+import com.lutysoft.scabs.modelo.conexao.Conexao;
+
+/**
+ *
+ * @author 55629
+ */
+public class FaccaoDao {
+    
+    private final Conexao conexao;
+    
+
+    public FaccaoDao() {
+        this.conexao = null;
+    }
+    
+    
+}
