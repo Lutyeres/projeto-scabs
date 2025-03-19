45e63951b8877d5cb8995cc1e93b692365dd752c
Savyo Lutyeres <lutyeres@gmail.com>
2025-03-19 18:09:53 -0300

    Criação da classe e modelo dao

---
+++ SCABS/src/main/java/com/lutysoft/scabs/modelo/dominio/ControleFaccao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lutysoft.scabs.modelo.dominio;
+
+/**
+ *
+ * @author 55629
+ */
+public class ControleFaccao {
+    private Long idControleFaccao;
+    private Long idFaccao;
+    private String tipoCorte;
+    private int quantEntrada;
+    private int quantSaida;
+    
+}
